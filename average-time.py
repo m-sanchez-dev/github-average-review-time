461b3c742ebe0a156a0ff3742e6bf1518c2acfff
Mikel Sanchez <mikelsanchezmart@gmail.com>
2024-02-22 15:05:14 +0000

    code clean up and improve performance

--- average-time.py
+++ average-time.py
@@ -7,18 +7,20 @@ from datetime import datetime, time, timedelta
 # Load environment variables from .env file
 load_dotenv()
 
-access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
-repo_owner = os.environ.get("GITHUB_REPO_OWNER")
-repo_name = os.environ.get("GITHUB_REPO_NAME")
 working_hours_start = time(int(os.environ.get("WORKING_HOURS_START", 7)), 0)
 working_hours_end = time(int(os.environ.get("WORKING_HOURS_END", 20)), 0)
-latest_date = datetime.strptime(os.environ.get("LATEST_DATE", "2023-12-31"), "%Y-%m-%d")
 
-if not access_token or not repo_owner or not repo_name:
-    print(
-        "Please set GITHUB_ACCESS_TOKEN, GITHUB_REPO_OWNER, and GITHUB_REPO_NAME environment variables."
-    )
-    exit()
+
+def check_env_variables():
+    access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
+    repo_owner = os.environ.get("GITHUB_REPO_OWNER")
+    repo_name = os.environ.get("GITHUB_REPO_NAME")
+
+    if not access_token or not repo_owner or not repo_name:
+        print(
+            "Please set GITHUB_ACCESS_TOKEN, GITHUB_REPO_OWNER, and GITHUB_REPO_NAME environment variables."
+        )
+        exit()
 
 
 def calculate_working_hours(
@@ -35,110 +37,143 @@ def calculate_working_hours(
     )
 
 
-def get_approval_time_for_pr(pr_number, access_token):
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/issues/{pr_number}/timeline"
+def get_approval_time_for_pr(pr_number):
+    access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
+    repo_owner = os.environ.get("GITHUB_REPO_OWNER")
+    repo_name = os.environ.get("GITHUB_REPO_NAME")
+
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls/{pr_number}/reviews"
     headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        timeline_data = response.json()
+    if not response.status_code == 200:
+        return None
 
-        for event in timeline_data:
-            if (
-                "state" in event
-                and event["state"] == "approved"
-                and "event" in event
-                and event["event"] == "reviewed"
-            ):
-                return datetime.strptime(event["submitted_at"], "%Y-%m-%dT%H:%M:%SZ")
+    timeline_data = response.json()
 
-    return None
+    for event in timeline_data:
+        if event["state"] == "APPROVED":
+            return datetime.strptime(event["submitted_at"], "%Y-%m-%dT%H:%M:%SZ")
 
 
-# Specify parameters for the GitHub API request
-url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
-params = {
-    "state": "closed",
-    "per_page": 1000,  # Number of pull requests per page
-    "page": 1,
-    "sort": "created",
-    "direction": "desc",  # Sort in descending order (newest first)
-}
-headers = {"Authorization": f"Bearer {access_token}"}
+def filter_pull_requests_by_date(pull_requests):
+    filtered_pull_requests = []
+    latest_date = datetime.strptime(
+        os.environ.get("LATEST_DATE", "2022-12-31"), "%Y-%m-%d"
+    )
+    print(len(pull_requests))
+    for pr in pull_requests:
+
+        created_at = datetime.strptime(pr["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        if created_at >= latest_date:
+            filtered_pull_requests.append(pr)
+
+    return filtered_pull_requests, len(pull_requests) > len(filtered_pull_requests)
+
+
+def get_all_pull_requests():
+    repo_owner = os.environ.get("GITHUB_REPO_OWNER")
+    repo_name = os.environ.get("GITHUB_REPO_NAME")
+    access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
+    print("Retrieving pull requests...")
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
+    params = {
+        "state": "closed",
+        "per_page": 100,  # Adjust as needed
+        "page": 1,  # Start from the first page
+        "sort": "created",
+        "direction": "desc",  # Sort in descending order (newest first)
+    }
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    all_pull_requests = []
+
+    while True:
+        response = requests.get(url, params=params, headers=headers)
+
+        if not response.status_code == 200:
+            print(
+                f"Failed to retrieve pull requests. Status code: {response.status_code}"
+            )
+            break
+
+        pull_requests = response.json()
+        if not pull_requests:
+            break
+
+        filtered_pull_requests, removed_old_pull_requests = (
+            filter_pull_requests_by_date(pull_requests)
+        )
+
+        all_pull_requests.extend(filtered_pull_requests)
+        print(f"{len(all_pull_requests)} PRs retrieved")
+
+        if removed_old_pull_requests:
+            break
+
+        # Move to the next page
+        params["page"] += 1
+
+    print(f"Retrieved {len(all_pull_requests)} pull requests")
+    return all_pull_requests
+
+
+check_env_variables()
 
 
 # Accumulate approval times across all pages
 total_approval_times = {}
-overDate = False
-while True:
-    response = requests.get(url, params=params, headers=headers)
 
-    if not response.status_code == 200:
-        print("Failed to retrieve pull requests. Status code:", response.status_code)
-        break
+pull_requests = get_all_pull_requests()
 
-    print("Retrieved page", params["page"], "of pull requests")
-    pull_requests = response.json()
-    if not pull_requests or overDate:
-        break  # No more pages to retrieve
+for pr in pull_requests:
+    created_at = datetime.strptime(pr["created_at"], "%Y-%m-%dT%H:%M:%SZ")
 
-    for pr in pull_requests:
-        created_at = datetime.strptime(pr["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+    author = pr["user"]["login"]
 
-        if created_at < latest_date:
-            overDate = True
-            break
-            # Exit the loop if the created_at date is before the
-            # specified latest date
+    if not pr["merged_at"]:
+        continue  # Skip if the pull request has not been merged
 
-        author = pr["user"]["login"]
+    pr_number = pr["number"]
+    approval_time = get_approval_time_for_pr(pr_number)
 
-        if not pr["merged_at"]:
-            continue  # Skip if the pull request has not been merged
+    if not approval_time:
+        continue
 
-        pr_number = pr["number"]
-        approval_time = get_approval_time_for_pr(pr_number, access_token)
+    # Initialize adjusted approval duration
+    approval_duration = 0
 
-        if not approval_time:
-            continue
+    # Iterate through days between submission and approval
+    current_day = created_at.date()
 
-        # Initialize adjusted approval duration
-        approval_duration = 0
+    while current_day <= approval_time.date():
+        # Determine the relevant working hours for the current day
+        current_start_time = max(
+            created_at, datetime.combine(current_day, working_hours_start)
+        )
+        current_end_time = min(
+            approval_time, datetime.combine(current_day, working_hours_end)
+        )
 
-        # Iterate through days between submission and approval
-        current_day = created_at.date()
+        # Calculate the time spent within working hours for the current day
+        current_duration = calculate_working_hours(
+            current_start_time,
+            current_end_time,
+            working_hours_start,
+            working_hours_end,
+        )
 
-        while current_day <= approval_time.date():
-            # Determine the relevant working hours for the current day
-            current_start_time = max(
-                created_at, datetime.combine(current_day, working_hours_start)
-            )
-            current_end_time = min(
-                approval_time, datetime.combine(current_day, working_hours_end)
-            )
+        # Add the time spent on the current day to the total approval
+        # duration
+        approval_duration += current_duration
 
-            # Calculate the time spent within working hours for the current day
-            current_duration = calculate_working_hours(
-                current_start_time,
-                current_end_time,
-                working_hours_start,
-                working_hours_end,
-            )
+        # Move to the next day
+        current_day += timedelta(days=1)
 
-            # Add the time spent on the current day to the total approval
-            # duration
-            approval_duration += current_duration
+    if author not in total_approval_times:
+        total_approval_times[author] = []
 
-            # Move to the next day
-            current_day += timedelta(days=1)
-
-        if author not in total_approval_times:
-            total_approval_times[author] = []
-
-        total_approval_times[author].append(approval_duration)
-
-    # Move to the next page
-    params["page"] += 1
+    total_approval_times[author].append(approval_duration)
 
 
 # Display the total average approval times
